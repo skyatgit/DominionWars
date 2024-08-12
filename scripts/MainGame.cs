5e57d8595b7851cc356e822320515befa60aa0f9
molingyu <z1522716486@hotmail.com>
2024-08-12 21:30:41 +0800

    feat: add GameManager

--- scripts/MainGame.cs
+++ scripts/MainGame.cs
@@ -1,3 +1,4 @@
+using DominionWars.Manager;
 using Godot;
 
 namespace DominionWars;
@@ -7,10 +8,16 @@ public partial class MainGame : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GameManager.Start();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GameManager.MainLoop();
+		if (GameManager.IsStop)
+		{
+			GetTree().Quit();
+		}
 	}
 }
